e636757a5b44ec3e1badc2440eab6052633af5da
rvzko <ravza.korkmaz@rub.de>
2021-02-05 23:38:52 +0100

    8: homework 1; not finished

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                System.Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -14,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         public InMemoryCarDal()
         {
             _cars = new List<Car>() {
-            new Car { Id = 1, BrandId = 1, ColorId = 111, ModelYear = 2020, DailyPrice = 300, Description = "Dacia Duster" },
-            new Car { Id = 2, BrandId = 1, ColorId = 112, ModelYear = 2019, DailyPrice = 280, Description = "Opel Corsa" },
-            new Car { Id = 3, BrandId = 2, ColorId = 113, ModelYear = 2019, DailyPrice = 270, Description = "Renault Megane" },
-            new Car { Id = 4, BrandId = 3, ColorId = 114, ModelYear = 2016, DailyPrice = 240, Description = "Fiat Linea" },
-            new Car { Id = 5, BrandId = 3, ColorId = 115, ModelYear = 2015, DailyPrice = 210, Description = "Renault Symbol" }
+            new Car { CarId = 1, BrandId = 1, ColorId = 111, ModelYear = 2020, DailyPrice = 300, Description = "Dacia Duster" },
+            new Car { CarId = 2, BrandId = 1, ColorId = 112, ModelYear = 2019, DailyPrice = 280, Description = "Opel Corsa" },
+            new Car { CarId = 3, BrandId = 2, ColorId = 113, ModelYear = 2019, DailyPrice = 270, Description = "Renault Megane" },
+            new Car { CarId = 4, BrandId = 3, ColorId = 114, ModelYear = 2016, DailyPrice = 240, Description = "Fiat Linea" },
+            new Car { CarId = 5, BrandId = 3, ColorId = 115, ModelYear = 2015, DailyPrice = 210, Description = "Renault Symbol" }
             };
         }
 
@@ -30,8 +31,13 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Car car)
         {
-            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
             _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Car> GetAll()
@@ -39,14 +45,19 @@ namespace DataAccess.Concrete.InMemory
             return _cars;
         }
 
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Car> GetById(int id)
         {
-            return _cars.Where(c => c.Id == id).ToList();
+            return _cars.Where(c => c.CarId == id).ToList();
         }
 
         public void Update(Car car)
         {
-            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
             carToUpdate.BrandId = car.BrandId;
             carToUpdate.ColorId = car.ColorId;
             carToUpdate.ModelYear = car.ModelYear;
