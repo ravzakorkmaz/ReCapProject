3e17951c97c274ad63d677bd70fce9f917209c80
rvzko <ravza.korkmaz@rub.de>
2021-02-20 17:03:12 +0100

    12 : Homework

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    class CarValidator:AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarName).NotEmpty();
+            RuleFor(c => c.CarName.Length).GreaterThanOrEqualTo(2).WithMessage("Araba ismi en az 2 karakterli olmalidir");
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+        }
+    }
+}
